2fb47ee2a5503825faf3030d77c4f557b705f1a2
775842767 <13071159748@yicai.com>
2016-06-17 11:53:47 +0800

    Git Test

--- thr/AppDelegate.m
+++ thr/AppDelegate.m
@@ -8,7 +8,12 @@
 
 #import "AppDelegate.h"
 
+@class ViewController;
+
 @interface AppDelegate ()
+
+
+@property (strong, nonatomic) ViewController *viewController;
 
 @end
 
@@ -16,9 +21,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+       
+//    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+//    [self.window makeKeyAndVisible];
     // Override point for customization after application launch.
     return YES;
 }
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
